474816a7c0776b3ace81e1e3009d384a36664f1b
Raghav Sharma <raghavusharmaa@gmail.com>
2026-05-22 01:08:22 +0530

    The data from API call is properly fetched. And ready to use furter in Processor

--- api/fetcher.py
+++ api/fetcher.py
@@ -1,22 +1,58 @@
 import requests
 def get_crypto_prices(coins:list,currency):
     url = "https://api.coingecko.com/api/v3/simple/price"
-    cleaned_coins = []
-    for coin in coins:
-        cleaned_coins.append(coin.strip())
-    final_coin = ",".join(cleaned_coins)
-    url += f"?ids={final_coin}&vs_currencies={currency.strip()}"
+
+    final_coin = ",".join(coins)
+    url += f"?ids={final_coin}&vs_currencies={currency}"
 
     try:
-        response = requests.get(url)
+        response = requests.get(url,timeout=5)
         if response.status_code == 200:
             data = response.json()
-            return data
-        return None
-    except Exception as e:
-        print("API FAILED!! ", e)
-        return None
+            return {
+                "success": True,
+                "data": data,
+                "error": None
+            }
+        else :
+            return {
+            "success":False,
+            "data": None,
+            "error": "API returned error response"
+        }
+    except requests.exceptions.Timeout:
+        # timeout case
+        return {
+                "success":False,
+                "data": None,
+                "error": "Request Timed out"
+            }
+
+    except requests.exceptions.ConnectionError:
+        # no internet
+        return {
+            "success":False,
+            "data": None,
+            "error": "Connection error"
+        }
+
+    except requests.exceptions.RequestException:
+        # other request-related issues
+        return {
+            "success":False,
+            "data": None,
+            "error": "Unexpected API response"
+        }
+
+
+
 if __name__ == "__main__":
     coins = input("Enter cryptocoins: ").lower().split(',')
+    final_coins =[]
+    for coin in coins:
+        coin = coin.strip()
+        if not coin:
+            continue
+        final_coins.append(coin)
     currency = input("Enter exchange currency: ")
-    get_crypto_prices(coins,currency)
+    result = get_crypto_prices(final_coins,currency)
